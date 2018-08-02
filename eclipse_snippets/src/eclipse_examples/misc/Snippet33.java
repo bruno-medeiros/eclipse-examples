acfed9692711e2e74ff1dd639cb67881f38af24b
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2018-08-02 11:16:03 +0100

    advance to section 8 of tutorial
    Signed-off-by: Bruno Medeiros <bruno.do.medeiros@gmail.com>

---
+++ eclipse_snippets/src/eclipse_examples/misc/Snippet33.java
@@ -0,0 +1,28 @@
+package eclipse_examples.misc;
+
+/*
+ * DirectoryDialog example snippet: prompt for a directory
+ *
+ * For a list of all SWT example snippets see
+ * http://www.eclipse.org/swt/snippets/
+ */
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
+public class Snippet33 {
+
+public static void main (String [] args) {
+	Display display = new Display ();
+	Shell shell = new Shell (display);
+	shell.open ();
+	DirectoryDialog dialog = new DirectoryDialog (shell, SWT.OPEN);
+	String platform = SWT.getPlatform();
+	dialog.setFilterPath (platform.equals("win32") ? "C:\\" : "/");
+	System.out.println ("RESULT=" + dialog.open ());
+	while (!shell.isDisposed()) {
+		if (!display.readAndDispatch ()) display.sleep ();
+	}
+	display.dispose ();
+}
+
+}
